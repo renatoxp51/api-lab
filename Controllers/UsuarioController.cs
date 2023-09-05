eec23adff931f79dc6da0e67d6d4493d9072cb08
renatoxp51 <reenato.sl09@gmail.com>
2023-09-04 21:00:14 -0300

    conexão com banco de dados

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,6 +1,37 @@
-﻿namespace LabReserva.Controllers
+﻿using LabReserva.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabReserva.Controllers
 {
-    public class UsuarioController
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
     {
+        private readonly ReservaLabContext _context; 
+
+        public UsuarioController(ReservaLabContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("testeconnection")]
+        public IActionResult TestConnection()
+        {
+            try
+            {
+                _context.Database.OpenConnection();
+                _context.Database.CloseConnection();
+
+                return Ok("Conexão Está Funcionando!");
+            }
+
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Erro ao conectar ao banco de dados!");
+            }
+        }
+
     }
 }
--- Data/ReservaLabContext.cs
+++ Data/ReservaLabContext.cs
@@ -1,6 +1,12 @@
-﻿namespace LabReserva.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LabReserva.Data
 {
-    public class ReservaLabContext
+    public class ReservaLabContext : DbContext 
     {
+        // construtor
+        public ReservaLabContext(DbContextOptions<ReservaLabContext> options) : base(options) { }
+
+
     }
 }
